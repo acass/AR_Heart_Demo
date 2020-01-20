4e3abaf90c5206ed0126f2c2c7e18fc115e2f68c
Allen Cass <acass@crossmedia411.com>
2020-01-20 10:33:34 -0800

    Added files to git

---
+++ Assets/VideoController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour, ITrackableEventHandler {
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    public VideoPlayer mVideoPlayer;
+    public GameObject mHandTarget;
+
+    protected virtual void Start() {
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy() {
+        
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+        
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + 
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackingLost();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+
+    protected virtual void OnTrackingFound() {
+
+        mVideoPlayer.Play();
+        mHandTarget.SetActive(false);
+    }
+
+
+    protected virtual void OnTrackingLost() {
+
+        mVideoPlayer.Pause();
+        mHandTarget.SetActive(true);
+    }
+
+}
